3b6f012f3458d8cf104ab8c2b2ff145de1a26ba3
David M. <damckeel13@gmail.com>
2026-07-02 22:42:53 -0500

    build simple server bound to localhost:8080

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/da-mckeel/chirpy-project
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	log.Fatal(server.ListenAndServe())
+
+}
