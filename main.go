b9dd6e991154cc52c0c63c3254b99e5feb6f2011
David M. <damckeel13@gmail.com>
2026-07-05 21:34:21 -0500

    Added image to serve, added readiness endpoint

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	server := &http.Server{
 		Handler: mux,
